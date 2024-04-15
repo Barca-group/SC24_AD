80e910c4fc862e523e00b6a51a24d87c41f8bf19
Jorge <jg4@iastate.edu>
2024-04-15 04:17:21 -0400

    better file arrangement

--- rtat_benefits/run_all.sh
+++ rtat_benefits/run_all.sh
@@ -1,14 +1,5 @@
 #!/bin/bash 
 
 
-module load gcc
-module load rocm/5.4.3
-module load hdf5
-module load magma
-module load cmake
-export HDF5_USE_FILE_LOCKING=FALSE
-
-srun -N 1 --ntasks=2 --gpus-per-node=1 -c 2 ./exess  paracetamol_no_rtat.json
-srun -N 1 --ntasks=2 --gpus-per-node=1 -c 2 ./exess  paracetamol_wrtat.json 
-srun -N 1 --ntasks=2 --gpus-per-node=1 -c 2 ./exess  urea_no_rtat.json 
-srun -N 1 --ntasks=2 --gpus-per-node=1 -c 2 ./exess  urea_wrtat.json
+srun -N 1 --ntasks=2 --gpus-per-node=1 -c 2 ./exess  no_rtat.json
+srun -N 1 --ntasks=2 --gpus-per-node=1 -c 2 ./exess  rtat.json 
