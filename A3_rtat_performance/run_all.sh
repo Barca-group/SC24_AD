7a40d2c509d9b37e5700cc811ba7dcb2fa87326a
Jorge <jg4@iastate.edu>
2024-04-16 03:06:54 -0400

    rtat

--- A3_rtat_performance/run_all.sh
+++ A3_rtat_performance/run_all.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 
 
-srun -N 1 --ntasks=2 --gpus-per-node=1 -c 2 ./exess  no_rtat.json
-srun -N 1 --ntasks=2 --gpus-per-node=1 -c 2 ./exess  rtat.json 
+srun -N 1 --ntasks=2 --gpus-per-node=1 -c 2 ./exess  nortat_paracetamol.json
+srun -N 1 --ntasks=2 --gpus-per-node=1 -c 2 ./exess  rtat_paracetamol.json 
+srun -N 1 --ntasks=2 --gpus-per-node=1 -c 2 ./exess  nortat_urea.json
+srun -N 1 --ntasks=2 --gpus-per-node=1 -c 2 ./exess  rtat_urea.json 
